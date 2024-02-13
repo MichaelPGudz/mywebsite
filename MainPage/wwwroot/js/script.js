4b646b94b16ebacf5dba89a0892e2c9fe59ad322
Michael Gudz <michaelp.gudz@gmail.com>
2024-02-13 18:03:16 +0100

    Added favicons, script and organized wwwroot folder

---
+++ MainPage/wwwroot/js/script.js
@@ -0,0 +1,6 @@
+﻿function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+
+    // Scroll smoothly to the section
+    section.scrollIntoView({ behavior: 'smooth' });
+}
