1c6eb94c43b0b37e477cfa97d67e1aec066918d0
Michael Gudz <michaelp.gudz@gmail.com>
2024-02-17 20:23:26 +0100

    Update NavMenu to use MainPageLibrary JS injection class

--- MainPage/Components/Layout/NavMenu.razor.js
+++
@@ -1,6 +0,0 @@
-﻿export function scrollToSection(sectionId) {
-    const section = document.getElementById(sectionId);
-
-    // Scroll smoothly to the section
-    section.scrollIntoView({ behavior: 'smooth' });
-}
