60e86df7f154a4d69e51b08c1a803b48aa255011
Michael Gudz <michaelp.gudz@gmail.com>
2024-02-23 01:35:55 +0100

    Deleted redundant test apps

--- BlazorApp/MainPage/wwwroot/js/script.js
+++
@@ -1,6 +0,0 @@
-﻿function scrollToSection(sectionId) {
-    const section = document.getElementById(sectionId);
-
-    // Scroll smoothly to the section
-    section.scrollIntoView({ behavior: 'smooth' });
-}
