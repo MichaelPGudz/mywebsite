db5318536a3b438b9b3aed3960efe50fdde7b7a4
Michael Gudz <michaelp.gudz@gmail.com>
2024-03-02 00:48:36 +0100

    #2 Added ApplicationDbContext

---
+++ MainPage.Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MainPage.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MainPage.Infrastructure.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+    }
+}
