eb98f6e0a251704f26525f0edc268d006dce701a
Michael Gudz <michaelp.gudz@gmail.com>
2024-03-02 00:49:26 +0100

    #2 Created ExperienceRepository for handling quries/commands from db

---
+++ MainPage.Infrastructure/Repositories/ExperienceRepository.cs
@@ -0,0 +1,29 @@
+﻿using MainPage.Domain.Entities;
+using MainPage.Domain.Repositories;
+using MainPage.Infrastructure.Database;
+
+namespace MainPage.Infrastructure.Repositories
+{
+    public sealed class ExperienceRepository : IExperienceRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ExperienceRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Task Add(Experience experience)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Experience>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Experience> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
