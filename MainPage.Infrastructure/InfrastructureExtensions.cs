1b9f21b97b7166debaf84c7727a8fea0b3fa2a77
Michael Gudz <michaelp.gudz@gmail.com>
2024-03-22 23:23:46 +0100

    #14 Added DI Extension methods to register services into projects

---
+++ MainPage.Infrastructure/InfrastructureExtensions.cs
@@ -0,0 +1,24 @@
+﻿using MainPage.Domain.Services;
+using MainPage.Infrastructure.Client;
+using MainPage.Infrastructure.Database;
+using MainPage.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MainPage.Infrastructure
+{
+    public static class InfrastructureExtensions
+    {
+        public static IServiceCollection AddDatabaseContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContextFactory<ApplicationDbContext>(options =>
+                    options.UseSqlServer(configuration.GetConnectionString("MainPageConnetion")));
+
+        public static IServiceCollection AddServerServices(this IServiceCollection services) =>
+            services.AddScoped<IExperienceService, ServerExperienceService>();
+
+        public static IServiceCollection AddClientServices(this IServiceCollection services) =>
+            services.AddScoped<IExperienceService, ClientExperienceService>();
+
+    }
+}
